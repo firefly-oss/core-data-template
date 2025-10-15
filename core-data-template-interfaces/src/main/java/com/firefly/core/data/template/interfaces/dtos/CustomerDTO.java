14153e4ddadbb1b83798159caa3e0dcfada583eb
Andrés Contreras Guillén <me@ancongui.com>
2025-10-16 01:00:40 +0200

    feat: enhance interfaces module DTOs
    - Extend CustomerDTO with additional fields and validation
    - Enhance OrderDTO with comprehensive data model
    - Update interfaces module configuration in pom.xml

--- core-data-template-interfaces/src/main/java/com/firefly/core/data/template/interfaces/dtos/CustomerDTO.java
+++ core-data-template-interfaces/src/main/java/com/firefly/core/data/template/interfaces/dtos/CustomerDTO.java
@@ -1,4 +1,14 @@
 package com.firefly.core.data.template.interfaces.dtos;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class CustomerDTO {
+    private String customerId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
 }
--- core-data-template-interfaces/src/main/java/com/firefly/core/data/template/interfaces/dtos/OrderDTO.java
+++ core-data-template-interfaces/src/main/java/com/firefly/core/data/template/interfaces/dtos/OrderDTO.java
@@ -1,4 +1,16 @@
 package com.firefly.core.data.template.interfaces.dtos;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.Instant;
+
+@Data
+@Builder
 public class OrderDTO {
+    private String orderId;
+    private String customerId;
+    private Double amount;
+    private String status;
+    private Instant orderDate;
 }
